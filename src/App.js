fa7265bcc7224736a74da62a3981cc2d77a83f7e
Helen <filatovae047@gmail.com>
2025-04-15 11:35:12 +0500

    modified

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import { use, useCallback, useEffect, useState } from 'react';
 import './App.css';
 import video from './cooking-animation.mp4';
-
 import MyRecipeComponent from './MyRecipeComponent';
-
 
 function App () {
   
@@ -13,22 +11,19 @@ function App () {
   const MY_ID = "15120dae";
   const MY_KEY = "b1a9fbc89afe42d79a81a90f5de337c1"
 
-
   const [mySearch, setMySearch] = useState("");
   const [myRecipes, setMyRecipes] = useState([]);
-  const [wordSubmitted, setWordSubmitted] = useState("apple")
-  
+  const [wordSubmitted, setWordSubmitted] = useState("lemon")
 
 useEffect(() => {
   const getRecipe = async () => {
     const response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${wordSubmitted}&app_id=${MY_ID}&app_key=${MY_KEY}`);
     const data = await response.json();
-    setMyRecipes(data.hits)
-    console.log(data.hits)
+    setMyRecipes(data.hits);
+    console.log(data.hits);
   }
   getRecipe()
 }, [wordSubmitted])
-
 
 
 const myRecipeSearch = (e) => {
@@ -38,40 +33,40 @@ const myRecipeSearch = (e) => {
 
 const finalSearch = (e) => {
 e.preventDefault()
-setWordSubmitted(mySearch)
+setWordSubmitted(mySearch);
 }
-
 
   return(
     <div  className='App'>
-      <div className='gallery'>
+      <div className='heading'>
     <video autoPlay muted loop>
       <source src={video} type="video/mp4"/>
     </video>
     <h1>Find a Recipe</h1>
       </div>
 
-      <div className='gallery'>
-        <form onSubmit={finalSearch}>
+              <div>
+             <form onSubmit={finalSearch} className='container'>
           <input className='search' onChange={myRecipeSearch} value={mySearch}/>
-        </form>
-      </div>
-
-      <div className='gallery'>
-        <button className="buttonSearch" onClick="finalSearch">
-          <img src="https://img.icons8.com/fluency/48/000000/fry.png" alt="icon"/>
+          <button  className='buttonSearch' onClick={finalSearch}>
+          <img src="https://img.icons8.com/fluency/48/000000/fry.png" alt="icon" />
         </button>
+      </form>
       </div>
 
+   
+       
+
+<div className='cards'>
       {myRecipes.map(( element, index)=> (
         <MyRecipeComponent key={index}
         label={element.recipe.label}
          image={element.recipe.image} 
          calories={element.recipe.calories} 
          ingridients={element.recipe.ingredientLines}
-         mealType={element.recipe.mealType}
-         dishType={element.recipe.dishType}/>
+         mealType={element.recipe.mealType}/>
       ))}
+      </div>
       </div>
   )
 }
--- src/MyRecipeComponent.js
+++ src/MyRecipeComponent.js
@@ -4,22 +4,22 @@ function MyRecipeComponent ({label,image, calories, ingridients,  mealType, dish
 
 const [isVisible, setIsVisible] = useState(false);
 
-return(<div className="gallery">
+return(<div className="cards">
             
-                    
-                       
-                    <div className="content">
-                   <img src={image} alt="recipes" className="mainImage"/>
+                    <div className="card">
+                    <img src={image} alt="recipes" className="mainImage"/>
+                    <div className="container">
+                 
                   
 
                    
                     <h2>{label}</h2>
-                   
+                   </div>
                        
-                 
+                   <div className="container">
                     <p className="typeDish">{dishType}</p>
-                    
-                    
+                    </div>
+                    <div className="container">
                             <button  onClick={() => setIsVisible(!isVisible)} className="buttonShow">{isVisible ? "HIDE THE RECIPE" : "SHOW THE RECIPE"}</button>
                       
 
@@ -34,12 +34,15 @@ return(<div className="gallery">
                        
                     </ul>
 )}
+</div>
+<div className="container">
                     <p>{calories.toFixed()} calories</p>
+                   </div>
                    
-                   
+                   <div className="container">
                     <p>Meal type: { mealType} </p>
                     </div>
-                
+                    </div>
                 
             
         </div>
